a5832f1fcff1c220eecde2ada63ed680a802e92b
Julius Torralba <Julius.Torralba@GMail.com>
2024-02-25 09:03:43 -0700

    Include DEBUG class.

---
+++ DEBUG/DEBUG.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Standard
+{
+    public class DEBUG
+    {
+        public void Message(string _Class, string _Message)
+        {
+            Console.WriteLine("{0}: {1}", _Class, _Message);
+        }
+    }
+}
